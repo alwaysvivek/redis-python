b62d924c91f775bb13b8853fe75076f2eb2a33f6
alwaysvivek <vivekdagar@zohomail.in>
2025-12-15 17:00:00 +0530

    Bump version to 0.2.2 and sync pyproject.toml

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="resp-server",
-    version="0.2.0",
+    version="0.2.2",
     author="Vivek Dagar",
     author_email="vivek@example.com",
     description="A lightweight, embeddable, pure-Python Redis-compatible server for testing",
